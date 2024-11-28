0fe62e4c82465a6199a53768418dd3b1c0aa0abb
jrobgal <jrobgal@gmail.com>
2024-11-28 16:00:00 -0500

    Ejercicio GestionOdontologica interfaces y modelo

---
+++ GestionOdontologica2848524A/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    if (isset($_GET["accion"])) {
+        if ($_GET["accion"] == "asignar") {
+            require_once 'Vista/html/asignar.php';
+        }
+        if ($_GET["accion"] == "consultar") {
+            require_once 'Vista/html/consultar.php';
+        }
+        if ($_GET["accion"] == "cancelar") {
+            require_once 'Vista/html/cancelar.php';
+        }
+    } else {
+        require_once 'Vista/html/inicio.php';
+    }
+    ?>
+</body>
+
+</html>
